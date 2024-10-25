7f9ce243fff372e7b94f0dfb935310e836b0bfbc
stnDinus <111202214433@mhs.dinus.ac.id>
2024-10-25 20:21:06 +0700

    fix: reduce max pass length to comply with bcrypt

--- app/(sign)/signin/schema.ts
+++ app/(sign)/signin/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const schema = z.object({
   username: z.string().length(14),
-  password: z.string().min(8).max(128),
+  password: z.string().min(8).max(72),
 });
 
 export default schema;
